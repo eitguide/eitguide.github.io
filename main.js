de58caca82ff2166f30ec0f01a7a9be6934d8b7c
Nguyen Nghia <nghiaklpro195@gmail.com>
2020-03-23 08:51:49 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,121 @@
+
+var peer = null;
+var lastPeerId = null;
+
+console.log(peer);
+
+var localVideo = document.getElementById('localVideo');
+var remoteVideo = document.getElementById('remoteVideo');
+var idInput = document.getElementById('idUser');
+var idInputConnect = document.getElementById('idConnectWith');
+
+const mediaStreamConstraints = {
+    video: true,
+    audio: true,
+};
+
+var startCallButton = document.getElementById('startCallButton');
+startCallButton.onclick = function() {
+    makingCall();
+};
+
+function onCallListener() {
+    console.log('onCallListener call');
+    peer.on('call', function(call) {
+        console.log('on call stream: ' + call);
+        navigator.mediaDevices.getUserMedia(mediaStreamConstraints)
+            .then((mediaStream) => {
+                localVideo.srcObject = mediaStream;
+
+                call.on('stream', function(remoteStream) {
+                    console.log('on stream remote');;
+                    remoteVideo.srcObject = remoteStream;
+                });
+
+                call.answer(mediaStream);
+
+                
+            })
+            .catch((error) => {
+                console.log('Failed to get local stream' ,err);
+            })
+        }
+    );
+}
+
+function makingCall() {
+    navigator.mediaDevices.getUserMedia(mediaStreamConstraints)
+        .then(handleLocalStreamComming)
+        .catch(handleLocalMediaStreamError);
+}
+
+function handleLocalStreamComming(mediaStream) {
+    console.log(mediaStream);
+
+    localVideo.srcObject = mediaStream;
+
+    var connectWith = idInputConnect.value;
+    console.log("Makig a call to: ", connectWith);
+
+    var connectionMedia = peer.call(connectWith, mediaStream);
+
+    connectionMedia.on('stream', function(remoteStream) {
+        console.log('on stream comming:' + remoteStream);
+        remoteVideo.srcObject = remoteStream;
+    });
+
+    connectionMedia.on('error', function(error) {
+        console.log(err);
+    })
+
+    connectionMedia.on('close', function() {
+        console.log("close on connectionMedia");
+    })
+}
+
+function handleLocalMediaStreamError(error) {
+    console.log(error);
+}
+
+function initialize() {
+    // Create own peer object with connection to shared PeerJS server
+    peer = new Peer(null, {
+        debug: 2
+    });
+
+    peer.on('open', function (id) {
+        // Workaround for peer.reconnect deleting previous id
+        if (peer.id === null) {
+            console.log('Received null id from peer open');
+            peer.id = lastPeerId;
+        } else {
+            lastPeerId = peer.id;
+        }
+
+        idInput.value = id;
+        console.log('ID: ' + peer.id);
+    });
+
+    peer.on('disconnected', function () {
+        status.innerHTML = "Connection lost. Please reconnect";
+        console.log('Connection lost. Please reconnect');
+
+        // Workaround for peer.reconnect deleting previous id
+        peer.id = lastPeerId;
+        peer._lastServerId = lastPeerId;
+        peer.reconnect();
+    });
+    peer.on('close', function() {
+        conn = null;
+        status.innerHTML = "Connection destroyed. Please refresh";
+        console.log('Connection destroyed');
+    });
+    peer.on('error', function (err) {
+        console.log(err);
+        alert('' + err);
+    });
+};
+
+initialize();
+onCallListener();
+
