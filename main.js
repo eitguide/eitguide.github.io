279eae91392bb9a744f67b6357ff7e288b67a594
Nguyen Nghia <nghiaklpro195@gmail.com>
2020-03-23 15:32:37 +0700

    update code

--- main.js
+++ main.js
@@ -86,10 +86,10 @@ function initialize() {
         debug: 3,
         config: { 'iceServers': [
             {"urls": "stun:stun.l.google.com:19302"},
-		    {"urls": "stun:stun1.l.google.com:19302"},
-		    {"urls": "stun:stun2.l.google.com:19302"},
-		    {"urls": "stun:stun3.l.google.com:19302"},
-		    {"urls": "stun:stun4.l.google.com:19302"}
+		{"urls": "stun:stun1.l.google.com:19302"},
+		{"urls": "stun:stun2.l.google.com:19302"},
+		{"urls": "stun:stun3.l.google.com:19302"},
+		{"urls": "stun:stun4.l.google.com:19302"}
         ]
     }
     });
